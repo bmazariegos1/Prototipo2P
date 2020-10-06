f583eed0de768adc39d0272e940f0488a4afec8c
bmazariegos1 <69173659+bmazariegos1@users.noreply.github.com>
2020-10-06 12:25:52 -0600

    Contenedor MDI

---
+++ SegundoParcial/CapaVistaExamen/FrmContenedorMDI.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace CapaVistaExamen
+{
+    partial class FrmContenedorMDI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.catálogoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.prosesosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.herramientasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registroDeAlumnoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registroDeMaestroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.catálogoToolStripMenuItem,
+            this.prosesosToolStripMenuItem,
+            this.informaciónToolStripMenuItem,
+            this.herramientasToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // catálogoToolStripMenuItem
+            // 
+            this.catálogoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registroDeAlumnoToolStripMenuItem,
+            this.registroDeMaestroToolStripMenuItem});
+            this.catálogoToolStripMenuItem.Name = "catálogoToolStripMenuItem";
+            this.catálogoToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.catálogoToolStripMenuItem.Text = "Catálogo";
+            // 
+            // prosesosToolStripMenuItem
+            // 
+            this.prosesosToolStripMenuItem.Name = "prosesosToolStripMenuItem";
+            this.prosesosToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.prosesosToolStripMenuItem.Text = "Prosesos";
+            // 
+            // informaciónToolStripMenuItem
+            // 
+            this.informaciónToolStripMenuItem.Name = "informaciónToolStripMenuItem";
+            this.informaciónToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.informaciónToolStripMenuItem.Text = "Informes";
+            // 
+            // herramientasToolStripMenuItem
+            // 
+            this.herramientasToolStripMenuItem.Name = "herramientasToolStripMenuItem";
+            this.herramientasToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
+            this.herramientasToolStripMenuItem.Text = "Herramientas";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // registroDeAlumnoToolStripMenuItem
+            // 
+            this.registroDeAlumnoToolStripMenuItem.Name = "registroDeAlumnoToolStripMenuItem";
+            this.registroDeAlumnoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.registroDeAlumnoToolStripMenuItem.Text = "Registro de Alumno";
+            // 
+            // registroDeMaestroToolStripMenuItem
+            // 
+            this.registroDeMaestroToolStripMenuItem.Name = "registroDeMaestroToolStripMenuItem";
+            this.registroDeMaestroToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.registroDeMaestroToolStripMenuItem.Text = "Registro de Maestro";
+            // 
+            // FrmContenedorMDI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmContenedorMDI";
+            this.Text = "FrmContenedorMDI";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem catálogoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem prosesosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informaciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registroDeAlumnoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registroDeMaestroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem herramientasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+    }
+}
